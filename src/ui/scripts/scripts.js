3cb3ad3492a9720ed842a0e451de302b28ac7319
Fabian Nart <fabian.nart@gmail.com>
2023-07-27 10:59:51 +0200

    New feature: Update tables
    Co-Authored-By: NatashaDudi <71380803+NatashaDudi@users.noreply.github.com>

--- src/ui/scripts/scripts.js
+++ src/ui/scripts/scripts.js
@@ -4,18 +4,9 @@ const cancelButton = document.querySelector('#cancel');
 const shapeMenu = document.querySelector('#shape');
 const countInput = document.querySelector('#count');
 
-//on load function
-document.addEventListener("DOMContentLoaded", function() {
-    formValidation();
-});
-
 //initialize select menu
 selectMenu.init();
 
-//event listeners
-countInput.oninput = () => { formValidation(); }
-shapeMenu.onchange = () => { formValidation(); }
-createShapesButton.onclick = () => { createShapes(); }
 cancelButton.onclick = () => { cancel(); }
 
 //form validation
